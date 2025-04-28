694a62c1de4be992495dd21d79f968344fe2802c
MADMAN-Modding <mad@kmdtech.net>
2025-04-28 15:10:25 -0400

    will except user input as arduino path

--- robotics_2025/src/main/java/com/divinechild/Main.java
+++ robotics_2025/src/main/java/com/divinechild/Main.java
@@ -39,7 +39,11 @@ public class Main {
 
 
     public static void main(String[] args) throws IllegalStateException, IOException {
+        
+        Constants.arduinoPort = args.length > 0 ? args[0]: Constants.arduinoPort;
+        
         IODevice arduino = new FirmataDevice(Constants.arduinoPort);
+
 
         try {
             arduino.start();
